5903f761644f2c721ac534d875f44febc2275619
Rusty Leutz <rleutz@gmail.com>
2020-03-08 09:49:27 -0400

    minor change

--- character/views.py
+++ character/views.py
@@ -3,4 +3,4 @@ from .models import Character, CharacterData
 
 def character_list(request):
     characters = Character.objects.all().order_by('slug')
-    return render(request, 'character/character_list.html', {})
+    return render(request, 'character/character_list.html', {'characters': characters})
