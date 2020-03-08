b2dc6243aa74ec9657dd5eae3e83d25f8cade964
Rusty Leutz <rleutz@gmail.com>
2020-03-08 16:11:16 -0400

    W

--- character/views.py
+++ character/views.py
@@ -6,6 +6,7 @@ def character_list(request):
     return render(request, 'character/character_list.html', {'characters': characters})
 
 def character_detail(request, pk):
-    character = Character.objects.exclude(data_type_name='Slug').get(pk=pk)
+    character = Character.objects.get(pk=pk)
+    list = CharacterData.objects.exclude(data_type_name = 'Slug').all()
     name = CharacterData.objects.get(character_id=pk, data_type_name='Slug')
-    return render(request, 'character/character_detail.html', {'character': character, 'name': name})
+    return render(request, 'character/character_detail.html', {'character': character, 'name': name, 'list': list})
