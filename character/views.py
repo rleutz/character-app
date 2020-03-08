816a282932332887dedfc8624cfab37f9a48a68f
Rusty Leutz <rleutz@gmail.com>
2020-03-08 11:11:12 -0400

    minor change

--- character/views.py
+++ character/views.py
@@ -6,5 +6,6 @@ def character_list(request):
     return render(request, 'character/character_list.html', {'characters': characters})
 
 def character_detail(request, pk):
-    character = get_object_or_404(Character, pk=pk)
+    # c = get_object_or_404(Character, pk=pk)
+    character = c.characterdata_set.filter(pk=pk)
     return render(request, 'character/character_detail.html', {'character': character})
