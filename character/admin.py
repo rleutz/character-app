b51a0e789f39671c1b2dc62c0eade290548ab00b
Rusty Leutz <rleutz@gmail.com>
2020-03-08 06:08:38 -0400

    fixed admin.py

--- character/admin.py
+++ character/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Character, DataType, CharacterData
+from .models import Character, CharacterData
 
 admin.site.register(Character)
 admin.site.register(DataType)
