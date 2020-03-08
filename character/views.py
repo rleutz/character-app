7cbbb8826d5d2f73aec57bdf49433cfa73d76c3e
Rusty Leutz <rleutz@gmail.com>
2020-03-08 09:47:12 -0400

    minor change

--- character/views.py
+++ character/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Character, CharacterData
 
 def character_list(request):
+    characters = Character.objects.all().order_by('slug')
     return render(request, 'character/character_list.html', {})
