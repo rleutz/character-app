a02c7fab68ec6bf6e7daeffda6fa78817be6a478
Rusty Leutz <rleutz@gmail.com>
2020-03-10 21:27:59 -0400

    W

--- character/urls.py
+++ character/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.character_list_view, name='character_list'),
-    path('filter/', views.character_filter_view, name='character_filter'),
+    path('', views.character_list_view.as_view(), name='character_list'),
+    path('filter/', views.character_filter_view.as_view(), name='character_filter'),
     path('character/<int:pk>/', views.character_detail, name='character_detail'),
 ]
