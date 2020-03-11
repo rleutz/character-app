9090eb76a32db0b56954c07243d1820a35bf649b
Rusty Leutz <rleutz@gmail.com>
2020-03-10 20:32:10 -0400

    W

--- character/models.py
+++ character/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 class Character(models.Model):
     slug = models.SlugField(unique=True, max_length=255)
+    player = models.CharField(max_length=255, null=True, blank=True)
     mule_for = models.CharField(max_length=255, null=True, blank=True)
     character_main = models.BooleanField(default=False)
     image = models.ImageField(upload_to='profile_image', blank=True)
