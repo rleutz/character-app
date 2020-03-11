51e40a94d04115385347b3f889bc53d92b86d2e8
Rusty Leutz <rleutz@gmail.com>
2020-03-10 21:12:33 -0400

    W

--- character/urls.py
+++ character/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.character_list, name='character_list'),
+    path('filter/', views.character_filter, name='character_filter'),
     path('character/<int:pk>/', views.character_detail, name='character_detail'),
 ]
--- character/views.py
+++ character/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Character, CharacterData
 
+def character_filter(request):
+    characters = Character.objects.filter(player=self.owner).order_by('slug')
+    return render(request, 'character/character_list.html', {'characters': characters})
+
 def character_list(request):
     characters = Character.objects.all().order_by('slug')
     return render(request, 'character/character_list.html', {'characters': characters})
