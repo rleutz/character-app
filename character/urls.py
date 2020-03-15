4079d28d7488b8f984ad7eb06c988324e15228ac
Rusty Leutz <rleutz@gmail.com>
2020-03-15 13:52:10 -0400

    W

--- character/urls.py
+++ character/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.character_list_view.as_view(), name='character_list'),
-    path('/urls', views.url_list_view.as_view(), name='url_list'),
+    path('urls/', views.url_list_view.as_view(), name='url_list'),
     path('filter/', views.character_filter_view.as_view(), name='character_filter'),
     path('character/<int:pk>/', views.character_detail, name='character_detail'),
     path('character/<int:pk>/edit/', views.character_edit, name="character_edit"),
